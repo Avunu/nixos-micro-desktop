b64323f243f81d7728bad741125dc0a5f84ce957
Kevin Shenk <kevin@avu.nu>
2024-07-02 09:28:07 -0400

    Create local-flake.nix

---
+++ local-flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    microdesktop = {
+      url = "github:Avunu/nixos-micro-desktop";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, microdesktop }: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        microdesktop.nixosModules.microDesktop
+        ./hardware-configuration.nix
+        ({ config, lib, pkgs, ... }: {
+          networking.hostName = "myhostname";  # Replace with desired hostname
+
+          time.timeZone = "America/New_York";  # Replace with your timezone
+
+          i18n.defaultLocale = "en_US.UTF-8";
+
+          users.users.myuser = {  # Replace 'myuser' with desired username
+            isNormalUser = true;
+            extraGroups = [ "wheel" "networkmanager" "audio" "video" ];
+          };
+
+          environment.systemPackages = with pkgs; [
+            # Add any non-flatpak software you want on this particular machine
+          ];
+
+          system.stateVersion = "24.05";
+        })
+      ];
+    };
+  };
+}
