504130411fb4f7aa1327df913e645e53a3aa3a62
Kevin Shenk <kevin@avu.nu>
2024-07-03 13:08:15 -0400

    a few more packages

--- flake.nix
+++ flake.nix
@@ -276,6 +276,8 @@
           [
             dnsmasq
             gcr_4
+            glib
+            gnome-menus
             gnome-network-displays
             gst_all_1.gst-libav
             gst_all_1.gst-plugins-bad
@@ -284,6 +286,8 @@
             gst_all_1.gst-plugins-ugly
             gst_all_1.gst-vaapi
             gst_all_1.gstreamer
+            gtk3.out
+            xdg-user-dirs
           ]
         ];
         variables = {
