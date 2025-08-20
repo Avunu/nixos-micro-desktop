13204380c8550009aeb16e7088ebd1b65b4b6130
Kevin Shenk <kevin@avu.nu>
2025-08-20 09:14:52 -0400

    fix: try to fix shortcuts

--- flake.nix
+++ flake.nix
@@ -377,27 +377,26 @@
                           "Mod+Shift+I".action = "move-workspace-up";
 
                           # Numbered workspaces
-                          "Mod+1".action = { focus-workspace = 1; };
-                          "Mod+2".action = { focus-workspace = 2; };
-                          "Mod+3".action = { focus-workspace = 3; };
-                          "Mod+4".action = { focus-workspace = 4; };
-                          "Mod+5".action = { focus-workspace = 5; };
-                          "Mod+6".action = { focus-workspace = 6; };
-                          "Mod+7".action = { focus-workspace = 7; };
-                          "Mod+8".action = { focus-workspace = 8; };
-                          "Mod+9".action = { focus-workspace = 9; };
-                          "Mod+Ctrl+1".action = { move-column-to-workspace = 1; };
-                          "Mod+Ctrl+2".action = { move-column-to-workspace = 2; };
-                          "Mod+Ctrl+3".action = { move-column-to-workspace = 3; };
-                          "Mod+Ctrl+4".action = { move-column-to-workspace = 4; };
-                          "Mod+Ctrl+5".action = { move-column-to-workspace = 5; };
-                          "Mod+Ctrl+6".action = { move-column-to-workspace = 6; };
-                          "Mod+Ctrl+7".action = { move-column-to-workspace = 7; };
-                          "Mod+Ctrl+8".action = { move-column-to-workspace = 8; };
-                          "Mod+Ctrl+9".action = { move-column-to-workspace = 9; };
+                          "Mod+1".action.focus-workspace = 1;
+                          "Mod+2".action.focus-workspace = 2;
+                          "Mod+3".action.focus-workspace = 3;
+                          "Mod+4".action.focus-workspace = 4;
+                          "Mod+5".action.focus-workspace = 5;
+                          "Mod+6".action.focus-workspace = 6;
+                          "Mod+7".action.focus-workspace = 7;
+                          "Mod+8".action.focus-workspace = 8;
+                          "Mod+9".action.focus-workspace = 9;
+                          "Mod+Ctrl+1".action.move-column-to-workspace = 1;
+                          "Mod+Ctrl+2".action.move-column-to-workspace = 2;
+                          "Mod+Ctrl+3".action.move-column-to-workspace = 3;
+                          "Mod+Ctrl+4".action.move-column-to-workspace = 4;
+                          "Mod+Ctrl+5".action.move-column-to-workspace = 5;
+                          "Mod+Ctrl+6".action.move-column-to-workspace = 6;
+                          "Mod+Ctrl+7".action.move-column-to-workspace = 7;
+                          "Mod+Ctrl+8".action.move-column-to-workspace = 8;
+                          "Mod+Ctrl+9".action.move-column-to-workspace = 9;
 
                           # Window layout
-                          "Mod+Comma".action = "consume-window-into-column";
                           "Mod+Period".action = "expel-window-from-column";
                           "Mod+R".action = "switch-preset-column-width";
                           "Mod+F".action = "maximize-column";
@@ -405,10 +404,10 @@
                           "Mod+C".action = "center-column";
 
                           # Column and window resizing
-                          "Mod+Minus".action = { set-column-width = "-10%"; };
-                          "Mod+Equal".action = { set-column-width = "+10%"; };
-                          "Mod+Shift+Minus".action = { set-window-height = "-10%"; };
-                          "Mod+Shift+Equal".action = { set-window-height = "+10%"; };
+                          "Mod+Minus".action.set-column-width = "-10%";
+                          "Mod+Equal".action.set-column-width = "+10%";
+                          "Mod+Shift+Minus".action.set-window-height = "-10%";
+                          "Mod+Shift+Equal".action.set-window-height = "+10%";
 
                           # Screenshots
                           "Print".action = "screenshot";
@@ -421,8 +420,8 @@
                           "Mod+Shift+Slash".action = "show-hotkey-overlay";
 
                           # Default applications
-                          "Mod+T".action = { spawn = ["alacritty"]; };
-                          "Mod+D".action = { spawn = ["fuzzel"]; };
+                          "Mod+T".action.spawn = ["alacritty"];
+                          "Mod+D".action.spawn = ["fuzzel"];
                         };
                       }
                     ];
