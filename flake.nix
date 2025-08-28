ab4db2401112c80d83db04e86b7543164a62cf83
Kevin Shenk <kevin@avu.nu>
2025-08-28 03:18:54 +0000

    feat: XDG_SESSION_DESKTOP

--- flake.nix
+++ flake.nix
@@ -333,6 +333,7 @@
                       SDL_VIDEODRIVER = "wayland";
                       XCURSOR_THEME = "Adwaita";
                       XDG_CURRENT_DESKTOP = "niri";
+                      XDG_SESSION_DESKTOP = "niri";
                       # XDG_DATA_DIRS = "$XDG_DATA_DIRS:$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share";
                       XDG_SESSION_TYPE = "wayland";
                     };
