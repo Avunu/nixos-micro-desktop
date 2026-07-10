50aa136c9e9e30cc8b4481e5ca7c45da89b11597
Kevin Shenk <kevin@avu.nu>
2026-07-10 11:23:50 -0400

    refactor: remove deprecated gst-vaapi

--- flake.nix
+++ flake.nix
@@ -464,7 +464,6 @@
                     gst_all_1.gst-plugins-base
                     gst_all_1.gst-plugins-good
                     gst_all_1.gst-plugins-ugly
-                    gst_all_1.gst-vaapi
                     gst_all_1.gstreamer
                     gtk4.out
                     # kdePackages.breeze
